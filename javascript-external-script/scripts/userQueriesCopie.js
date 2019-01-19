910030a1401edec2ba72217e8a60725765278c29
Nadou Djellali <nadiawho@MacBook-Pro-de-Nadou.local>
2019-01-19 23:49:44 +0100

    user queries

---
+++ javascript-external-script/scripts/userQueriesCopie.js
@@ -0,0 +1,6 @@
+// simple concatenation and prompt message
+const firstname = prompt('What\'s your lastname? ')
+const lastname = prompt('What\'s your firstname ?')
+const age = prompt('How old are you? ')
+
+console.log(`Bonjour ${firstname} ${lastname}. Vous avez ${age} ans.`)
