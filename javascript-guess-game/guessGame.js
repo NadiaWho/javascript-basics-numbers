4fb9d60f9686d8898be03245f3b2ff62ed6474ed
Nadou Djellali <nadiawho@MacBook-Pro-de-Nadou.local>
2019-01-20 17:59:47 +0100

    guess game

---
+++ javascript-guess-game/guessGame.js
@@ -0,0 +1,12 @@
+const numToGuess = prompt('Give me a number between 0 to 20')
+    if (numToGuess <= 10) {
+        alert('le chiffre est inférieur à celui à deviner')
+    }
+    else if (numToGuess > 11) {
+        alert('le chiffre est supérieur à celui à deviner')
+    }
+
+    else if (numToGuess !== 11) {
+        alert('Bien joué ! C\'était bien le chiffre 11')
+    }
+    
